e068f807308e892d3d8de783a32b4b8514395b07
yavkata999 <yavor.kr@gmail.com>
2025-11-27 10:16:45 +0200

    changed some of the text

--- content/products/torfeni-substrati-hobi.ts
+++ content/products/torfeni-substrati-hobi.ts
@@ -1,3 +1,4 @@
+// content/products/torfeni-substrati-hobi.ts
 import type { Product } from "./types";
 
 export const torfeniSubstratiHobiProducts: Product[] = [
@@ -12,9 +13,9 @@ export const torfeniSubstratiHobiProducts: Product[] = [
       "Торфеният субстрат е специално разработен за отглеждане на стайни и балконски цветя, както и за подобряване на качеството на почвата. Осигурява отлична аерация и воден баланс за кореновата система.\n\nТЕХНИЧЕСКИ ХАРАКТЕРИСТИКИ:\n\n• Влажност: до 65%\n• pH (H2O): 5,8 - 6,3\n• NPK: 100-250, 120-300, 150-400 мг/л.\n• Микроелементи: Fe, Mn, Cu, B, Zn и др.\n• Fiba Zorb: 100 - 200 мл./м3\n• Електропроводимост: 1,7 - 2,0 mS/см",
     packaging: "10 л, 20 л, 50 л",
     images: [
-      "https://res.cloudinary.com/dggelqxji/image/upload/v1729766335/large_za_cvetya_10l_9af982d7b2.png",
-      "https://res.cloudinary.com/dggelqxji/image/upload/v1729766335/large_za_cvetya_20l_c64d22a4f5.png",
-      "https://res.cloudinary.com/dggelqxji/image/upload/v1729766335/large_za_cvetya_50l_10d6a50e78.png",
+      "https://res.cloudinary.com/dggelqxji/image/upload/v1729766334/za_cvetya_10l_9af982d7b2.png",
+      "https://res.cloudinary.com/dggelqxji/image/upload/v1729766335/za_cvetya_20l_c64d22a4f5.png",
+      "https://res.cloudinary.com/dggelqxji/image/upload/v1729766334/za_cvetya_50l_10d6a50e78.png",
     ],
     features: [
       "Балансирано pH (5.8 - 6.3)",
@@ -39,7 +40,7 @@ export const torfeniSubstratiHobiProducts: Product[] = [
       "Специализирана смес за декоративни видове, млади дръвчета и храсти. Съдържа необходимите хранителни вещества за силен старт и устойчиво развитие на кореновата система.\n\nТЕХНИЧЕСКИ ХАРАКТЕРИСТИКИ:\n\n• Влажност: до 65%\n• pH (H2O): 5,0 - 5,8\n• NPK: 80-150, 120-200, 150-300 мг/л.\n• Микроелементи: Fe, Mn, Cu, B, Zn и др.\n• Fiba Zorb: 100 - 200 мл./м3\n• Електропроводимост: 1,5 - 1,8 mS/см",
     packaging: "50 л",
     images: [
-      "https://res.cloudinary.com/dggelqxji/image/upload/v1729766657/large_decorativni_rastenoya_50l_2664bf1c3b.png",
+      "https://res.cloudinary.com/dggelqxji/image/upload/v1729766656/decorativni_rastenoya_50l_2664bf1c3b.png",
     ],
     features: [
       "pH 5.0 - 5.8",
@@ -60,7 +61,7 @@ export const torfeniSubstratiHobiProducts: Product[] = [
       "Висококачествен торфен субстрат, създаден да осигури оптимална среда за корените на стайните растения. Гарантира продължителен растеж и здраве благодарение на добавените торове.\n\nТЕХНИЧЕСКИ ХАРАКТЕРИСТИКИ:\n\n• Влажност: до 65%\n• pH (H2O): 5,5 - 6,5\n• NPK: 100-150, 120-200, 150-350 мг/л.\n• Микроелементи: Fe, Mn, Cu, B, Zn и др.\n• Fiba Zorb: 100 - 200 мл./м3\n• Електропроводимост: 1,5 - 2,0 mS/cм",
     packaging: "20 л",
     images: [
-      "https://res.cloudinary.com/dggelqxji/image/upload/v1729766944/large_staini_rasteniya_20l_b38f5bed89.png",
+      "https://res.cloudinary.com/dggelqxji/image/upload/v1729766944/staini_rasteniya_20l_b38f5bed89.png",
     ],
     features: [
       "Оптимално pH 5.5 - 6.5",
@@ -85,7 +86,7 @@ export const torfeniSubstratiHobiProducts: Product[] = [
       "Специализирана смес за зеленчукопроизводство, осигуряваща богат хранителен състав за формиране на здрава реколта. Идеален за оранжерийно и открито отглеждане.\n\nТЕХНИЧЕСКИ ХАРАКТЕРИСТИКИ:\n\n• Влажност: до 65%\n• pH (H2O): 5,8 - 6,2\n• NPK: 100-250, 120-300, 150-400 мг/л.\n• Микроелементи: Fe, Mn, Cu, B, Zn и др.\n• Fiba Zorb: 100 - 200 мл./м3\n• Електропроводимост: 1,7 - 2,2 mS/см",
     packaging: "70 л",
     images: [
-      "https://res.cloudinary.com/dggelqxji/image/upload/v1729767046/large_zelenchuci_70l_c0537daf2b.png",
+      "https://res.cloudinary.com/dggelqxji/image/upload/v1729767046/zelenchuci_70l_c0537daf2b.png",
     ],
     features: [
       "Високо съдържание на NPK",
@@ -110,10 +111,10 @@ export const torfeniSubstratiHobiProducts: Product[] = [
       "Изработен по специална рецепта с екологично чист торф, обогатен с хранителни добавки и микроелементи. Подходящ за широк спектър от градински и балконски приложения.\n\nТЕХНИЧЕСКИ ХАРАКТЕРИСТИКИ:\n\n• Влажност: до 65%\n• pH (H2O): 5,8 - 6,3\n• NPK: 100-250, 120-300, 150-400 мг/л.\n• Микроелементи: Fe, Mn, Cu, B, Zn и др.\n• Fiba Zorb: 100 - 200 мл./м3\n• Електропроводимост: 1,6 - 1,9 mS/см",
     packaging: "5 л, 10 л, 20 л, 70 л",
     images: [
-      "https://res.cloudinary.com/dggelqxji/image/upload/v1729767136/large_zelenchuci_i_zvetya_5l_d48850a0f3.png",
-      "https://res.cloudinary.com/dggelqxji/image/upload/v1729767136/large_zelenchuci_i_zvetya_10l_ca4947a3b5.png",
-      "https://res.cloudinary.com/dggelqxji/image/upload/v1729767136/large_zelenchuci_i_zvetya_20l_ba6d4cd1ef.png",
-      "https://res.cloudinary.com/dggelqxji/image/upload/v1729767137/large_zelenchuci_i_zvetya_70l_50872b4860.png",
+      "https://res.cloudinary.com/dggelqxji/image/upload/v1729767136/zelenchuci_i_zvetya_5l_d48850a0f3.png",
+      "https://res.cloudinary.com/dggelqxji/image/upload/v1729767136/zelenchuci_i_zvetya_10l_ca4947a3b5.png",
+      "https://res.cloudinary.com/dggelqxji/image/upload/v1729767136/zelenchuci_i_zvetya_20l_ba6d4cd1ef.png",
+      "https://res.cloudinary.com/dggelqxji/image/upload/v1729767136/zelenchuci_i_zvetya_70l_50872b4860.png",
     ],
     features: [
       "Универсално приложение",
@@ -138,8 +139,8 @@ export const torfeniSubstratiHobiProducts: Product[] = [
       "Субстрат с фина структура, предназначен за прецизно засяване. Осигурява нежна среда за младите корени с по-ниска солева концентрация за предотвратяване на изгаряния.\n\nТЕХНИЧЕСКИ ХАРАКТЕРИСТИКИ:\n\n• Влажност: до 65%\n• pH (H2O): 5,2 - 6,5\n• NPK: 50-120, 80-190, 100-260 мг/л.\n• Микроелементи: Fe, Mn, Cu, B, Zn и др.\n• Fiba Zorb: 100 - 200 мл./м3\n• Електропроводимост: 0,5 - 1,2 mS/см",
     packaging: "20 л, 70 л",
     images: [
-      "https://res.cloudinary.com/dggelqxji/image/upload/v1729767217/large_razsadi_20l_649bdf68a0.png",
-      "https://res.cloudinary.com/dggelqxji/image/upload/v1729767218/large_razsadi_70l_7597d38a8b.png",
+      "https://res.cloudinary.com/dggelqxji/image/upload/v1729767217/razsadi_20l_649bdf68a0.png",
+      "https://res.cloudinary.com/dggelqxji/image/upload/v1729767219/razsadi_70l_7597d38a8b.png",
     ],
     features: [
       "Ниска електропроводимост (EC)",
@@ -164,7 +165,7 @@ export const torfeniSubstratiHobiProducts: Product[] = [
       "Субстрат, обогатен със специална глина и пясък за осигуряване на перфектен дренаж, жизненоважен за кактусите. Подходящ за Epiphylium, Schlumberger, Mammiliaria и други сукуленти.\n\nТЕХНИЧЕСКИ ХАРАКТЕРИСТИКИ:\n\n• Влажност: до 65%\n• pH (H2O): 5,5 - 6,5\n• NPK: 50-100, 100-150, 150-200 мг/л.\n• Микроелементи: Fe, Mn, Cu, B, Mo, Zn и др.\n• Fiba Zorb: 100 - 200 мл./м3\n• Електропроводимост: 0,8 - 1,3 mS/cм",
     packaging: "10 л",
     images: [
-      "https://res.cloudinary.com/dggelqxji/image/upload/v1729767395/large_kaktus_10l_24560c043a.png",
+      "https://res.cloudinary.com/dggelqxji/image/upload/v1729767395/kaktus_10l_24560c043a.png",
     ],
     features: [
       "Отличен дренаж с пясък",
@@ -185,7 +186,7 @@ export const torfeniSubstratiHobiProducts: Product[] = [
       "Специализирана кисела почва (pH 4.2 - 4.8) за растения, които не понасят варовик. Осигурява правилната среда за усвояване на желязо и други елементи от специфичните видове.\n\nТЕХНИЧЕСКИ ХАРАКТЕРИСТИКИ:\n\n• Влажност: до 65%\n• pH (H2O): 4,2 - 4,8\n• NPK: 80-180, 120-250, 150-350 мг/л.\n• Микроелементи: Fe, Mn, Cu, B, Zn и др.\n• Fiba Zorb: 100 - 200 мл./м3\n• Електропроводимост: 1,3 - 2,0 mS/см",
     packaging: "50 л",
     images: [
-      "https://res.cloudinary.com/dggelqxji/image/upload/v1729767487/large_azaliya_i_rododendron_20l_8589b25594.png",
+      "https://res.cloudinary.com/dggelqxji/image/upload/v1729767487/azaliya_i_rododendron_20l_8589b25594.png",
     ],
     features: [
       "Ниско pH (4.2 - 4.8)",
@@ -206,7 +207,7 @@ export const torfeniSubstratiHobiProducts: Product[] = [
       "Рецепта с повишено съдържание на желязо за предотвратяване на хлороза и стимулиране на ярък цвят. Идеален за висящи кошници и балконски сандъчета, изложени на слънце.\n\nТЕХНИЧЕСКИ ХАРАКТЕРИСТИКИ:\n\n• Влажност: до 65%\n• pH (H2O): 5,5 - 6,5\n• NPK: 100-200, 120-250, 150-400 мг/л.\n• Микроелементи: Fe (0,1 - 0,2 кг./м3), Mn, Cu, B, Zn\n• Fiba Zorb: 100 - 200 мл./м3\n• Електропроводимост: 1,5 - 2,2 mS/cм",
     packaging: "20 л",
     images: [
-      "https://res.cloudinary.com/dggelqxji/image/upload/v1729767576/large_gradinski_balkonski_zvetya_20l_10d94b6e01.png",
+      "https://res.cloudinary.com/dggelqxji/image/upload/v1729767576/gradinski_balkonski_zvetya_20l_10d94b6e01.png",
     ],
     features: [
       "Добавено желязо (Fe)",
@@ -231,7 +232,7 @@ export const torfeniSubstratiHobiProducts: Product[] = [
       "Порест субстрат, който лесно се усвоява от растенията и задоволява техните биоенергийни нужди за висок добив. Оптимизиран за домати и пипер.\n\nТЕХНИЧЕСКИ ХАРАКТЕРИСТИКИ:\n\n• Влажност: до 65%\n• pH (H2O): 5,5 - 6,5\n• NPK: 100-200, 120-300, 150-400 мг/л.\n• Микроелементи: Fe, Mn, Cu, B, Mo, Zn и др.\n• Fiba Zorb: 100 - 200 мл./м3\n• Електропроводимост: 1,0 - 2,0 mS/cм",
     packaging: "20 л",
     images: [
-      "https://res.cloudinary.com/dggelqxji/image/upload/v1729767657/large_domati_i_piper_20l_8e34491463.png",
+      "https://res.cloudinary.com/dggelqxji/image/upload/v1729767657/domati_i_piper_20l_8e34491463.png",
     ],
     features: [
       "Висока порьозност",
@@ -252,7 +253,7 @@ export const torfeniSubstratiHobiProducts: Product[] = [
       "Специална рецепта, която подобрява задържането на влага в кореновата зона на тревата и намалява отмиването на торове. Идеален за стартиране на нови тревни площи.",
     packaging: "70 л",
     images: [
-      "https://res.cloudinary.com/dggelqxji/image/upload/v1729767696/large_zatrevyavane_70l_00942b9a20.png",
+      "https://res.cloudinary.com/dggelqxji/image/upload/v1729767696/zatrevyavane_70l_00942b9a20.png",
     ],
     features: [
       "Задържа влагата",
@@ -273,7 +274,7 @@ export const torfeniSubstratiHobiProducts: Product[] = [
       "Висококачествен неутрализиран торф със сертифицирани органични добавки. Осигурява екологично чиста среда за растеж до 6 седмици без допълнително торене.",
     packaging: "50 л",
     images: [
-      "https://res.cloudinary.com/dggelqxji/image/upload/v1729767739/small_ekodurpeta_50l_6bc0321fa4.png",
+      "https://res.cloudinary.com/dggelqxji/image/upload/v1729767739/ekodurpeta_50l_6bc0321fa4.png",
     ],
     features: [
       "Сертифициран за БИО земеделие",
@@ -298,8 +299,8 @@ export const torfeniSubstratiHobiProducts: Product[] = [
       "Произведен от висококачествен сегментиран торф, този продукт е предназначен за декоративно покритие, потискане на плевели и чисто оформяне на пространството.",
     packaging: "10 л, 50 л",
     images: [
-      "https://res.cloudinary.com/dggelqxji/image/upload/v1729767813/large_dekoraciya_i_oformyane_na_grobishtni_parkove_10l_6f4a66338e.png",
-      "https://res.cloudinary.com/dggelqxji/image/upload/v1729767812/large_dekoraciya_i_oformyane_na_grobishtni_parkove_50l_46b513a8f3.png",
+      "https://res.cloudinary.com/dggelqxji/image/upload/v1729767813/dekoraciya_i_oformyane_na_grobishtni_parkove_10l_6f4a66338e.png",
+      "https://res.cloudinary.com/dggelqxji/image/upload/v1729767812/dekoraciya_i_oformyane_na_grobishtni_parkove_50l_46b513a8f3.png",
     ],
     features: [
       "Естетичен тъмен цвят",
@@ -324,8 +325,8 @@ export const torfeniSubstratiHobiProducts: Product[] = [
       "Специално обогатен за засаждане на растения и озеленяване. Осигурява нужните хранителни вещества за цветя и ниски храсти в условия на открити паркове.",
     packaging: "20 л, 50 л",
     images: [
-      "https://res.cloudinary.com/dggelqxji/image/upload/v1729767878/large_zasajdane_i_aranjirane_na_grobishtni_parkove_20l_83d338e782.png",
-      "https://res.cloudinary.com/dggelqxji/image/upload/v1729767878/large_zasajdane_i_aranjirane_na_grobishtni_parkove_50l_e8ec26e024.png",
+      "https://res.cloudinary.com/dggelqxji/image/upload/v1729767878/zasajdane_i_aranjirane_na_grobishtni_parkove_20l_83d338e782.png",
+      "https://res.cloudinary.com/dggelqxji/image/upload/v1729767878/zasajdane_i_aranjirane_na_grobishtni_parkove_50l_e8ec26e024.png",
     ],
     features: [
       "Обогатен състав",
@@ -350,7 +351,7 @@ export const torfeniSubstratiHobiProducts: Product[] = [
       "Гъвкаво решение за професионалисти и големи градини. Всеки от нашите субстрати може да бъде пакетиран в този обем по заявка на клиента.",
     packaging: "80 л",
     images: [
-      "https://res.cloudinary.com/dggelqxji/image/upload/v1729767931/medium_universal_80l_bd200d300f.png",
+      "https://res.cloudinary.com/dggelqxji/image/upload/v1729767931/universal_80l_bd200d300f.png",
     ],
     features: [
       "Икономичен обем",
@@ -392,7 +393,7 @@ export const torfeniSubstratiHobiProducts: Product[] = [
       "Съставен от бавно разграждащи се материали, този мулч поддържа почвата рохкава и влажна. Съдържа дървесни влакна, които стимулират кореновата система и отблъскват някои вредители.",
     packaging: "60 л",
     images: [
-      "https://res.cloudinary.com/dggelqxji/image/upload/v1729768092/large_torfen_mulch_60l_15b664d049.png",
+      "https://res.cloudinary.com/dggelqxji/image/upload/v1729768092/torfen_mulch_60l_15b664d049.png",
     ],
     features: [
       "Органичен произход",
@@ -417,7 +418,7 @@ export const torfeniSubstratiHobiProducts: Product[] = [
       "Висококачествена смес с борова кора, агроперлит и сфагнум мъх, приготвена по австрийска рецепта. Осигурява отлична въздухопропускливост, критична за корените на орхидеите.",
     packaging: "5 л",
     images: [
-      "https://res.cloudinary.com/dggelqxji/image/upload/v1729768150/medium_orhideya_5l_22412035a4.png",
+      "https://res.cloudinary.com/dggelqxji/image/upload/v1729768150/orhideya_5l_22412035a4.png",
     ],
     features: [
       "Висока въздухопропускливост",
@@ -442,7 +443,7 @@ export const torfeniSubstratiHobiProducts: Product[] = [
       "Направен от силно фрагментиран хумусен торф и органични торове. Идеален за отглеждане на босилек, мента, розмарин и други билки в домашни условия.",
     packaging: "5 л",
     images: [
-      "https://res.cloudinary.com/dggelqxji/image/upload/v1729768190/large_bilki_5l_9d74f4fa8e.png",
+      "https://res.cloudinary.com/dggelqxji/image/upload/v1729768190/bilki_5l_9d74f4fa8e.png",
     ],
     features: [
       "100% Органичен състав",
@@ -464,10 +465,10 @@ export const torfeniSubstratiHobiProducts: Product[] = [
     shortDescription:
       "Кисела почва за ерика, калуна и други киселолюбиви видове.",
     longDescription:
-      "Създава идеалния киселинен диапазон (pH 3.5-4.5) за специфични растения. Отлична порьозност и добавени дълготрайни торове за успешен растеж.",
+      "Създава идеалния киселинен диапазон (pH 3.5-4.5) за специфични растения. Отлична порьозност и добавени дълготрайни торове за успешен растеж.\n\nТЕХНИЧЕСКИ ХАРАКТЕРИСТИКИ:\n\n• Влажност: до 65%\n• pH (H2O): 3,5 - 4,5\n• NPK: 14-16-18 и 12-11-18 (минерални торове)\n• Електропроводимост: 0,8 - 1,3 mS/cm",
     packaging: "20 л",
     images: [
-      "https://res.cloudinary.com/dggelqxji/image/upload/v1729768232/large_erika_20l_bbdfa1b1f1.png",
+      "https://res.cloudinary.com/dggelqxji/image/upload/v1729768232/erika_20l_bbdfa1b1f1.png",
     ],
     features: [
       "Силно кисело pH (3.5 - 4.5)",
@@ -487,7 +488,7 @@ export const torfeniSubstratiHobiProducts: Product[] = [
       "Професионална смес с тор с контролирано освобождаване (до 4 месеца) и агроперлит. Гарантира здрави корени и липса на патогени при производството на ягоди.",
     packaging: "50 л",
     images: [
-      "https://res.cloudinary.com/dggelqxji/image/upload/v1729768277/large_yagodi_50l_bf5c0a8924.png",
+      "https://res.cloudinary.com/dggelqxji/image/upload/v1729768277/yagodi_50l_bf5c0a8924.png",
     ],
     features: [
       "Включен тор Osmocote Pro",
@@ -508,10 +509,10 @@ export const torfeniSubstratiHobiProducts: Product[] = [
     brand: "Durpeta",
     shortDescription: "Смес с дървесни фибри за иглолистни храсти и туи.",
     longDescription:
-      "Комбинация от торф и натрошена целулоза с кисело pH (4.3-5.2). Съдържа минерални торове и глина за стабилност и хранене на вечнозелени растения.",
+      "Комбинация от торф и натрошена целулоза с кисело pH (4.3-5.2). Съдържа минерални торове и глина за стабилност и хранене на вечнозелени растения.\n\nТЕХНИЧЕСКИ ХАРАКТЕРИСТИКИ:\n\n• pH (H2O): 4,3 - 5,2\n• NPK: 14-16-18 и 12-11-18\n• Електропроводимост: 0,8 - 1,3 mS/cm\n• Органично вещество: над 93%",
     packaging: "50 л",
     images: [
-      "https://res.cloudinary.com/dggelqxji/image/upload/v1729768314/small_iglolistni_i_dekorativni_rasteniya_50l_2be06ac0b6.png",
+      "https://res.cloudinary.com/dggelqxji/image/upload/v1729768314/iglolistni_i_dekorativni_rasteniya_50l_2be06ac0b6.png",
     ],
     features: [
       "Кисело pH (4.3 - 5.2)",
@@ -535,10 +536,10 @@ export const torfeniSubstratiHobiProducts: Product[] = [
       "Мощна органична смес (съотношение 4:4:2 компост/торф/тор), обогатена с вар и перлит. Идеална за възстановяване на изтощени почви и засаждане на овошки.",
     packaging: "5 л, 10 л, 50 л, 80 л",
     images: [
-      "https://res.cloudinary.com/dggelqxji/image/upload/v1729768382/large_pochven_kompost_50l_e8a73c4cf7.png",
-      "https://res.cloudinary.com/dggelqxji/image/upload/v1729768382/small_pochven_kompost_5l_2b7e45e44d.png",
-      "https://res.cloudinary.com/dggelqxji/image/upload/v1729768382/small_pochven_kompost_10l_d7d4a75181.png",
-      "https://res.cloudinary.com/dggelqxji/image/upload/v1729768382/large_pochven_kompost_80l_8d6f03d011.png",
+      "https://res.cloudinary.com/dggelqxji/image/upload/v1729768382/pochven_kompost_50l_e8a73c4cf7.png",
+      "https://res.cloudinary.com/dggelqxji/image/upload/v1729768382/pochven_kompost_5l_2b7e45e44d.png",
+      "https://res.cloudinary.com/dggelqxji/image/upload/v1729768382/pochven_kompost_10l_d7d4a75181.png",
+      "https://res.cloudinary.com/dggelqxji/image/upload/v1729768382/pochven_kompost_80l_8d6f03d011.png",
     ],
     features: [
       "Високо органично съдържание",
@@ -562,8 +563,8 @@ export const torfeniSubstratiHobiProducts: Product[] = [
       "Киселинен, лек торф от високи блата, смесен с борова кора за оптимална среда за корените на боровинките. Осигурява нужната киселинност и дренаж.",
     packaging: "70 л, 150 л",
     images: [
-      "https://res.cloudinary.com/dggelqxji/image/upload/v1729768432/large_borovinki_70l_f423cdb849.png",
-      "https://res.cloudinary.com/dggelqxji/image/upload/v1729768432/small_borovinki_150l_931aba5433.png",
+      "https://res.cloudinary.com/dggelqxji/image/upload/v1729768432/borovinki_70l_f423cdb849.png",
+      "https://res.cloudinary.com/dggelqxji/image/upload/v1729768432/borovinki_150l_931aba5433.png",
     ],
     features: [
       "Силно киселинен",
