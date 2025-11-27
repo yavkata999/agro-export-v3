f5567df50910452ce73487b561f78efbdc0005a3
yavkata999 <yavor.kr@gmail.com>
2025-11-27 14:09:19 +0200

    Add Turkish seedling trays category and product pages

---
+++ content/products/formi-za-razsad-turcziya.ts
@@ -0,0 +1,460 @@
+import type { Product } from "./types";
+
+export const formiZaRazsadTurciaProducts: Product[] = [
+  {
+    id: "formi-turcia-24-square",
+    category: "formi-za-razsad-turcziya",
+    name: "24 гнезда [квадратни]",
+    brand: "Agro Export",
+    shortDescription: "Табла с 24 големи квадратни гнезда (7х7х7 см).",
+    longDescription:
+      "Здрава турска форма за разсад с 24 квадратни гнезда. Големият обем на гнездото позволява отглеждането на добре развити растения с мощна коренова система преди разсаждане в земята.\n\nТЕХНИЧЕСКИ ХАРАКТЕРИСТИКИ:\n• Размер на таблата: 52 x 33 см\n• Размер на отвора: 7 x 7 x 7 см\n• Цвят: Черен",
+    packaging: "100 бр./кашон",
+    images: [
+      "https://res.cloudinary.com/dggelqxji/image/upload/v1729835591/formi_za_razsad_turciya_24_square_7b78e2ad19.png",
+    ],
+    features: [
+      "Голям обем на гнездото",
+      "Квадратна форма",
+      "Устойчива пластмаса",
+      "За силна коренова система",
+    ],
+    applications: [
+      "Домати и пипер",
+      "Патладжан",
+      "Декоративни храсти",
+    ],
+  },
+  {
+    id: "formi-turcia-24-round",
+    category: "formi-za-razsad-turcziya",
+    name: "24 гнезда [кръгли]",
+    brand: "Agro Export",
+    shortDescription: "Табла с 24 кръгли гнезда (Ф7 см).",
+    longDescription:
+      "Класическа форма с 24 кръгли клетки. Подходяща за зеленчуци и цветя, които изискват повече пространство за растеж.\n\nТЕХНИЧЕСКИ ХАРАКТЕРИСТИКИ:\n• Размер на таблата: 52 x 33 см\n• Размер на отвора: Ф7 x 7 см\n• Цвят: Черен",
+    packaging: "100 бр./кашон",
+    images: [
+      "https://res.cloudinary.com/dggelqxji/image/upload/v1729835654/formi_za_razsad_turciya_24_round_cbfacd1712.png",
+    ],
+    features: [
+      "Кръгли гнезда",
+      "Лесно вадене на разсада",
+      "Добър дренаж",
+      "Многократна употреба",
+    ],
+    applications: [
+      "Зеленчуков разсад",
+      "Саксийни цветя",
+      "Горски видове",
+    ],
+  },
+  {
+    id: "formi-turcia-28-round",
+    category: "formi-za-razsad-turcziya",
+    name: "28 гнезда [кръгли]",
+    brand: "Agro Export",
+    shortDescription: "Форма с 28 гнезда (Ф7 см).",
+    longDescription:
+      "Професионална табла за производство на разсад. Оптимизиран размер за средно големи растения.\n\nТЕХНИЧЕСКИ ХАРАКТЕРИСТИКИ:\n• Размер на таблата: 52 x 33 см\n• Размер на отвора: Ф7 x 6,5 см\n• Цвят: Черен",
+    packaging: "100 бр./кашон",
+    images: [
+      "https://res.cloudinary.com/dggelqxji/image/upload/v1729835739/formi_za_razsad_turciya_28_round_a81b035bd8.png",
+    ],
+    features: [
+      "Размер: 52 x 33 см",
+      "Брой гнезда: 28",
+      "Стабилна конструкция",
+      "Лесен транспорт",
+    ],
+    applications: [
+      "Зеленчукопроизводство",
+      "Цветарство",
+      "Оранжерии",
+    ],
+  },
+  {
+    id: "formi-turcia-32-round",
+    category: "formi-za-razsad-turcziya",
+    name: "32 гнезда [кръгли]",
+    brand: "Agro Export",
+    shortDescription: "Табла с 32 кръгли гнезда (Ф6 см).",
+    longDescription:
+      "Универсална форма с 32 гнезда, осигуряваща баланс между брой растения и обем на почвата.\n\nТЕХНИЧЕСКИ ХАРАКТЕРИСТИКИ:\n• Размер на таблата: 52 x 33 см\n• Размер на отвора: Ф6 x 7 см\n• Цвят: Черен",
+    packaging: "100 бр./кашон",
+    images: [
+      "https://res.cloudinary.com/dggelqxji/image/upload/v1729835795/formi_za_razsad_turciya_32_round_e13ca5554d.png",
+    ],
+    features: [
+      "Оптимална гъстота",
+      "Дълбочина 7 см",
+      "Кръгъл профил",
+      "Здрав материал",
+    ],
+    applications: [
+      "Пипер и домати",
+      "Зеле",
+      "Декоративни растения",
+    ],
+  },
+  {
+    id: "formi-turcia-35-round",
+    category: "formi-za-razsad-turcziya",
+    name: "35 гнезда [кръгли]",
+    brand: "Agro Export",
+    shortDescription: "Форма с 35 гнезда (Ф6.5 см).",
+    longDescription:
+      "Табла за разсад с 35 гнезда, малко по-широка от стандартните модели (53 см).\n\nТЕХНИЧЕСКИ ХАРАКТЕРИСТИКИ:\n• Размер на таблата: 53 x 33 см\n• Размер на отвора: Ф6,5 x 7 см\n• Цвят: Черен",
+    packaging: "100 бр./кашон",
+    images: [
+      "https://res.cloudinary.com/dggelqxji/image/upload/v1729835855/formi_za_razsad_turciya_35_round_5d381ce6f4.png",
+    ],
+    features: [
+      "По-широка табла (53см)",
+      "Дълбоки гнезда",
+      "Добра аерация",
+      "Професионално качество",
+    ],
+    applications: [
+      "Зеленчуци",
+      "Билки",
+      "Ягоди от семе",
+    ],
+  },
+  {
+    id: "formi-turcia-45-round",
+    category: "formi-za-razsad-turcziya",
+    name: "45 гнезда [кръгли]",
+    brand: "Agro Export",
+    shortDescription: "Табла с 45 гнезда (Ф5 см).",
+    longDescription:
+      "Компактна форма с 45 гнезда, идеална за масово производство на разсад с по-малка коренова система.\n\nТЕХНИЧЕСКИ ХАРАКТЕРИСТИКИ:\n• Размер на таблата: 53 x 33 см\n• Размер на отвора: Ф5 x 5,3 см\n• Цвят: Черен",
+    packaging: "100 бр./кашон",
+    images: [
+      "https://res.cloudinary.com/dggelqxji/image/upload/v1729835931/formi_za_razsad_turciya_45_round_8d0cc14e58.png",
+    ],
+    features: [
+      "Икономия на субстрат",
+      "Висока плътност",
+      "Кръгли отвори",
+      "Подходяща за пикиране",
+    ],
+    applications: [
+      "Марули и салати",
+      "Цветя (Тагетес, Петуния)",
+      "Подправки",
+    ],
+  },
+  {
+    id: "formi-turcia-48-square",
+    category: "formi-za-razsad-turcziya",
+    name: "48 гнезда [квадратни]",
+    brand: "Agro Export",
+    shortDescription: "Квадратна форма с 48 гнезда (5.5x5.5 см).",
+    longDescription:
+      "Популярна табла с квадратни гнезда, които позволяват по-добро използване на обема за кореновата система в сравнение с кръглите.\n\nТЕХНИЧЕСКИ ХАРАКТЕРИСТИКИ:\n• Размер на таблата: 52 x 36 см\n• Размер на отвора: 5,5 х 5,5 х 6,5 см\n• Цвят: Черен",
+    packaging: "100 бр./кашон",
+    images: [
+      "https://res.cloudinary.com/dggelqxji/image/upload/v1729836020/formi_za_razsad_turciya_48_square_d05c53e4b9.png",
+    ],
+    features: [
+      "Квадратни гнезда",
+      "Оптимален обем на корена",
+      "Широка табла (36 см)",
+      "Стабилна основа",
+    ],
+    applications: [
+      "Всички видове зеленчуци",
+      "Декоративни растения",
+      "Разсадници",
+    ],
+  },
+  {
+    id: "formi-turcia-60-round",
+    category: "formi-za-razsad-turcziya",
+    name: "60 гнезда [кръгли]",
+    brand: "Agro Export",
+    shortDescription: "Табла с 60 гнезда за интензивно производство.",
+    longDescription:
+      "Форма с висока плътност (60 гнезда), подходяща за култури с кратък престой в таблата или по-малки растения.\n\nТЕХНИЧЕСКИ ХАРАКТЕРИСТИКИ:\n• Размер на таблата: 52 x 33 см\n• Размер на отвора: Ф4,7 x 5 см\n• Цвят: Черен",
+    packaging: "50 бр./кашон",
+    images: [
+      "https://res.cloudinary.com/dggelqxji/image/upload/v1729836091/formi_za_razsad_turciya_60_round_c656e07e46.png",
+    ],
+    features: [
+      "Висока продуктивност",
+      "Икономичен размер",
+      "Добър дренаж",
+      "Лека конструкция",
+    ],
+    applications: [
+      "Листни зеленчуци",
+      "Ранен разсад",
+      "Цветя от семена",
+    ],
+  },
+  {
+    id: "formi-turcia-70-square-long",
+    category: "formi-za-razsad-turcziya",
+    name: "70 гнезда [квадратни дълбоки]",
+    brand: "Agro Export",
+    shortDescription: "Дълбока табла със 70 гнезда за развита коренова система.",
+    longDescription:
+      "Специализирана дълбока форма, идеална за култури, които развиват дълъг централен корен.\n\nТЕХНИЧЕСКИ ХАРАКТЕРИСТИКИ:\n• Размер на таблата: 52 x 33 см\n• Размер на отвора: 4,2 х 4,2 см\n• Цвят: Черен",
+    packaging: "100 бр./кашон",
+    images: [
+      "https://res.cloudinary.com/dggelqxji/image/upload/v1729836144/formi_za_razsad_turciya_70_square_long_457549f9f3.png",
+    ],
+    features: [
+      "Удължен профил на гнездото",
+      "Квадратна форма",
+      "Насочва корена надолу",
+      "Предпазва от навиване на корена",
+    ],
+    applications: [
+      "Горски разсад",
+      "Лози и овошки",
+      "Рози",
+    ],
+  },
+  {
+    id: "formi-turcia-84-square",
+    category: "formi-za-razsad-turcziya",
+    name: "84 гнезда [квадратни]",
+    brand: "Agro Export",
+    shortDescription: "Табла с 84 гнезда за масово производство.",
+    longDescription:
+      "Форма с 84 малки квадратни гнезда, осигуряваща отлична плътност на разсада върху единица площ.\n\nТЕХНИЧЕСКИ ХАРАКТЕРИСТИКИ:\n• Размер на таблата: 50 x 30 см\n• Размер на отвора: 3,5 х 3,5 х 4,8 см\n• Цвят: Черен",
+    packaging: "100 бр./кашон",
+    images: [
+      "https://res.cloudinary.com/dggelqxji/image/upload/v1729836210/formi_za_razsad_turciya_84_square_a2893ab5ec.png",
+    ],
+    features: [
+      "Компактен размер (50х30)",
+      "Висока гъстота",
+      "Квадратни гнезда",
+      "Икономия на място",
+    ],
+    applications: [
+      "Салати и марули",
+      "Целина",
+      "Дребни цветя",
+    ],
+  },
+  {
+    id: "formi-turcia-104-round-long",
+    category: "formi-za-razsad-turcziya",
+    name: "104 гнезда [кръгли дълбоки]",
+    brand: "Agro Export",
+    shortDescription: "Дълбока табла със 104 гнезда за разсад.",
+    longDescription:
+      "Многоклетъчна табла с дълбоки кръгли гнезда, предназначена за голям брой растения, изискващи дълбочина за корена.\n\nТЕХНИЧЕСКИ ХАРАКТЕРИСТИКИ:\n• Размер на таблата: 53 x 32 см\n• Размер на отвора: Ф3,2 х 5,5 см\n• Цвят: Черен",
+    packaging: "100 бр./кашон",
+    images: [
+      "https://res.cloudinary.com/dggelqxji/image/upload/v1729836757/formi_za_razsad_turciya_104_round_long_d7c192fc13.png",
+    ],
+    features: [
+      "Дълбок профил (5.5 см)",
+      "Тесен отвор",
+      "Висока продуктивност",
+      "Подходяща за резници",
+    ],
+    applications: [
+      "Тютюн",
+      "Лекарствени растения",
+      "Горски видове от семе",
+    ],
+  },
+  {
+    id: "formi-turcia-50-square",
+    category: "formi-za-razsad-turcziya",
+    name: "50 гнезда [квадратни]",
+    brand: "Agro Export",
+    shortDescription: "Класическа квадратна форма с 50 гнезда.",
+    longDescription:
+      "Стандартна форма за разсад с 50 квадратни гнезда. Отличен баланс за повечето зеленчукови култури.\n\nТЕХНИЧЕСКИ ХАРАКТЕРИСТИКИ:\n• Размер на таблата: 53 x 28 см\n• Размер на отвора: 5 х 5 х 5 см\n• Цвят: Черен",
+    packaging: "100 бр./кашон",
+    images: [
+      "https://res.cloudinary.com/dggelqxji/image/upload/v1729837121/formi_za_razsad_turciya_50_square_e38e883f22.png",
+    ],
+    features: [
+      "Универсален размер",
+      "Квадратни гнезда",
+      "Оптимален обем",
+      "Лесно обслужване",
+    ],
+    applications: [
+      "Домати",
+      "Пипер",
+      "Краставици",
+    ],
+  },
+  {
+    id: "formi-turcia-72-square",
+    category: "formi-za-razsad-turcziya",
+    name: "72 гнезда [квадратни]",
+    brand: "Agro Export",
+    shortDescription: "Квадратна форма със 72 гнезда.",
+    longDescription:
+      "Ефективна табла за производство на разсад със 72 гнезда. Подходяща за култури, които се разсаждат по-рано.\n\nТЕХНИЧЕСКИ ХАРАКТЕРИСТИКИ:\n• Размер на таблата: 53 x 28 см\n• Размер на отвора: 4 х 4 х 4,5 см\n• Цвят: Черен",
+    packaging: "100 бр./кашон",
+    images: [
+      "https://res.cloudinary.com/dggelqxji/image/upload/v1729837335/formi_za_razsad_turciya_72_square_60be196101.png",
+    ],
+    features: [
+      "Висока плътност",
+      "Квадратен профил",
+      "Икономия на торф",
+      "За професионалисти",
+    ],
+    applications: [
+      "Зеле и карфиол",
+      "Салати",
+      "Цветя",
+    ],
+  },
+  {
+    id: "formi-turcia-77-square-long",
+    category: "formi-za-razsad-turcziya",
+    name: "77 гнезда [квадратни дълбоки]",
+    brand: "Agro Export",
+    shortDescription: "Дълбока квадратна табла със 77 гнезда.",
+    longDescription:
+      "Специализирана форма с удължени квадратни гнезда (6.5 см дълбочина). Осигурява отлично развитие на корена.\n\nТЕХНИЧЕСКИ ХАРАКТЕРИСТИКИ:\n• Размер на таблата: 55 x 33 см\n• Размер на отвора: 4 х 4 х 6,5 см\n• Цвят: Черен",
+    packaging: "100 бр./кашон",
+    images: [
+      "https://res.cloudinary.com/dggelqxji/image/upload/v1729837417/formi_za_razsad_turciya_77_square_long_aaf4d63d46.png",
+    ],
+    features: [
+      "Дълбок профил",
+      "Голяма табла (55х33)",
+      "Насочване на корена",
+      "Здрава конструкция",
+    ],
+    applications: [
+      "Дървесни видове",
+      "Специфични зеленчуци",
+      "Резници",
+    ],
+  },
+  {
+    id: "formi-turcia-128-square",
+    category: "formi-za-razsad-turcziya",
+    name: "128 гнезда [квадратни]",
+    brand: "Agro Export",
+    shortDescription: "Табла със 128 малки квадратни гнезда.",
+    longDescription:
+      "Табла за масово производство на разсад от дребни семена. Квадратните гнезда осигуряват максимално използване на площта.\n\nТЕХНИЧЕСКИ ХАРАКТЕРИСТИКИ:\n• Размер на таблата: 53 x 28 см\n• Размер на отвора: 3 х 3 х 4,7 см\n• Цвят: Черен",
+    packaging: "100 бр./кашон",
+    images: [
+      "https://res.cloudinary.com/dggelqxji/image/upload/v1729837540/formi_za_razsad_turciya_128_square_08a3692d08.png",
+    ],
+    features: [
+      "Много висока плътност",
+      "За дребни семена",
+      "Автоматизирано пълнене",
+      "Икономична",
+    ],
+    applications: [
+      "Тютюн",
+      "Праз лук",
+      "Целина",
+    ],
+  },
+  {
+    id: "formi-turcia-podlojka",
+    category: "formi-za-razsad-turcziya",
+    name: "Универсална подложка",
+    brand: "Agro Export",
+    shortDescription: "Плътна подложка за турски форми за разсад.",
+    longDescription:
+      "Универсална тава (подложка), съвместима с размерите на турските форми за разсад. Използва се за долно поливане и транспорт, за да се избегне замърсяване.\n\nТЕХНИЧЕСКИ ХАРАКТЕРИСТИКИ:\n• Размер: 55 х 33 х 2,5 см\n• Цвят: Черен",
+    packaging: "100 бр./кашон",
+    images: [
+      "https://res.cloudinary.com/dggelqxji/image/upload/v1729837713/podlojka_75fe541129.png",
+    ],
+    features: [
+      "Съвместима с повечето форми",
+      "Задържа вода",
+      "Улеснява транспорта",
+      "Здрава пластмаса",
+    ],
+    applications: [
+      "Поливане на разсад",
+      "Транспорт",
+      "Хигиена в оранжерията",
+    ],
+  },
+  {
+    id: "formi-turcia-20-separatable",
+    category: "formi-za-razsad-turcziya",
+    name: "20 гнезда [разделящи се]",
+    brand: "Agro Export",
+    shortDescription: "Форма с 20 гнезда, които могат да се разделят.",
+    longDescription:
+      "Специализирана форма, при която гнездата (или групи от гнезда) могат да се отделят едно от друго. Това е идеално за продажба на разсад на дребно, без да се вади растението от почвата.\n\nТЕХНИЧЕСКИ ХАРАКТЕРИСТИКИ:\n• Брой гнезда: 20\n• Цвят: Черен\n• Тип: Разделящи се (на ленти или единични)",
+    packaging: "250 бр./кашон",
+    images: [
+      "https://res.cloudinary.com/dggelqxji/image/upload/v1729838382/formi_za_razsad_turciya_20_square_separatable_86fb055b71.png",
+    ],
+    features: [
+      "Разделящи се гнезда",
+      "Удобни за търговия",
+      "Запазват корена при продажба",
+      "Лесно манипулиране",
+    ],
+    applications: [
+      "Търговия с разсад",
+      "Пазари и агроаптеки",
+      "Любителски разсад",
+    ],
+  },
+  {
+    id: "formi-turcia-45-separatable",
+    category: "formi-za-razsad-turcziya",
+    name: "45 гнезда [разделящи се]",
+    brand: "Agro Export",
+    shortDescription: "Форма с 45 разделящи се квадратни гнезда.",
+    longDescription:
+      "Форма с 45 гнезда, проектирана за лесно разделяне. Позволява на производителите да продават разсада директно в контейнера, запазвайки го свеж.\n\nТЕХНИЧЕСКИ ХАРАКТЕРИСТИКИ:\n• Брой гнезда: 45\n• Цвят: Черен\n• Тип: Разделящи се",
+    packaging: "100 бр./кашон",
+    images: [
+      "https://res.cloudinary.com/dggelqxji/image/upload/v1729838498/formi_za_razsad_turciya_45_square_separatable_bcc688f9a0.png",
+    ],
+    features: [
+      "Възможност за разделяне",
+      "Търговски вид",
+      "Оптимизиран обем",
+      "Квадратни гнезда",
+    ],
+    applications: [
+      "Продажба на цветя",
+      "Зеленчуков разсад на дребно",
+      "Градински центрове",
+    ],
+  },
+  {
+    id: "formi-turcia-48-separatable",
+    category: "formi-za-razsad-turcziya",
+    name: "48 гнезда [разделящи се]",
+    brand: "Agro Export",
+    shortDescription: "Форма с 48 разделящи се гнезда.",
+    longDescription:
+      "Практична форма с 48 гнезда, която може да се нарязва или чупи на по-малки секции за удобство при продажба и транспорт на крайния клиент.\n\nТЕХНИЧЕСКИ ХАРАКТЕРИСТИКИ:\n• Брой гнезда: 48\n• Цвят: Черен\n• Тип: Разделящи се",
+    packaging: "150 бр./кашон",
+    images: [
+      "https://res.cloudinary.com/dggelqxji/image/upload/v1729838599/formi_za_razsad_turciya_48_square_separatable_5fbb93d513.png",
+    ],
+    features: [
+      "Гъвкава употреба",
+      "Разделяща се структура",
+      "Защита на корена",
+      "Идеални за пазара",
+    ],
+    applications: [
+      "Търговия с разсад",
+      "Сезонни разсади",
+      "Цветарство",
+    ],
+  },
+];
